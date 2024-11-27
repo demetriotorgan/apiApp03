e9b40e37876929224ba2f599ed5f2d70a7d7242a
demetriotorgan <demetriotorgan@hotmail.com>
2024-11-27 12:13:16 -0300

    cors

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.port || 5000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 
 mongoose
     .connect(process.env.MONGODB_URL)
