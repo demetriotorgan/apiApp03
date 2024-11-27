2132e47f73b9d0c18c39bad2c95e0a6711b66de5
demetriotorgan <demetriotorgan@hotmail.com>
2024-11-27 12:22:34 -0300

    cors2

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.port || 5000;
 app.use(express.json());
+
+app.use((req,res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();    
+  });
 app.use(cors({
     origin:"*"
 }));
