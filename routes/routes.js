1a4538280e0d7a8e7b916ddd808a0041538e8c11
demetriotorgan <demetriotorgan@hotmail.com>
2024-11-27 15:54:06 -0300

    corsVenda

--- routes/routes.js
+++ routes/routes.js
@@ -11,7 +11,7 @@ router.post('/produtos/delete', deleteProduto);
 router.post('/produtos/update', updateProduto);
 
 //venda
-router.get('/produtos', getVenda);
-router.post('/produtos', saveVenda);
+router.get('/venda', getVenda);
+router.post('/venda', saveVenda);
 
 module.exports = router;
