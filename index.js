d29b917862dc21793bcc6a038531c1180967a138
demetriotorgan <demetriotorgan@hotmail.com>
2024-11-27 16:40:06 -0300

    corslocal

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const app = express();
 const PORT = process.env.port || 5000;
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173/venda',    
+    origin:'http://localhost:5173',    
 }));
 app.use((req,res, next)=>{
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173/venda');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     next();    
   });
