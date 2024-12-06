2d89b422daf7aba0d5bab4c14f2aba7e738da355
demetriotorgan <demetriotorgan@hotmail.com>
2024-12-06 11:58:43 -0300

    atualizarStatus

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const { getProdutos, saveProduto, deleteProduto, updateProduto } = require('../controllers/estoqueControler');
-const { saveVenda, getVenda, deleteVenda } = require('../controllers/vendaControler');
+const { saveVenda, getVenda, deleteVenda, updateProdutoVendido } = require('../controllers/vendaControler');
 const { addPagamento, deletePagamento, updatePagamento } = require('../controllers/pagamentoControler');
 
 const router = Router();
@@ -15,6 +15,7 @@ router.post('/produtos/update', updateProduto);
 router.get('/produtos/venda', getVenda);
 router.post('/produtos/venda/save', saveVenda);
 router.delete('/produtos/venda/:vendaId', deleteVenda);
+router.put('/produtos/venda', updateProdutoVendido);
 
 //pagamentos
 router.put('/produtos/venda/:id/pagamentos', addPagamento);
